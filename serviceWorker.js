594fc08907948d6ef7b9224c4f8896a93fd3142b
solomon chigozie <solomon1chigozie@gmail.com>
2021-07-30 14:52:05 +0100

    new commit

--- serviceWorker.js
+++ serviceWorker.js
@@ -3,7 +3,10 @@ const assets = [
     "/",
     "index.html",
     "src/master.css",
-    "images/logo192.png"
+    "images/logo192.png",
+    "https://cdn.pixabay.com/photo/2012/11/02/13/02/car-63930_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2016/04/01/12/16/car-1300629_960_720.png",
+    "https://cdn.pixabay.com/photo/2014/10/22/17/22/car-498244_960_720.jpg"
 ];
 
 self.addEventListener("install", installEvent => {
