d464d9878d679c8300c5425229f74a3624db25c9
solomon chigozie <solomon1chigozie@gmail.com>
2021-07-30 14:32:28 +0100

    new commit

--- src/index.js
+++ src/index.js
@@ -1,40 +1,19 @@
-if("serviceWorker" in navigator){
-    navigator.serviceWorker.register("sw.js").then(registration => {
-        console.log("SW Registered");
-        console.log(registration);
-    }).catch(error => {
-        console.log("SW Registration failed");
-        console.log(error);
-    })
-}
+// if("serviceWorker" in navigator){
+//     navigator.serviceWorker.register("sw.js").then(registration => {
+//         console.log("SW Registered");
+//         console.log(registration);
+//     }).catch(error => {
+//         console.log("SW Registration failed");
+//         console.log(error);
+//     })
+// }
 
-//install button
-let deferredPrompt;
-const addBtn = document.querySelector('.add-button');
 
-addBtn.style.display = 'none';
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI to notify the user they can add to home screen
-  addBtn.style.display = 'block';
-
-  addBtn.addEventListener('click', () => {
-    // hide our user interface that shows our A2HS button
-    addBtn.style.display = 'none';
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-      } else {
-        console.log('User dismissed the A2HS prompt');
-      }
-      deferredPrompt = null;
-    });
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("./serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err));
   });
-});
+}
